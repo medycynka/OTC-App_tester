4be9a306298eed08c8673bde331e32056e75ac56
Szymon Peszek <44410552+medycynka@users.noreply.github.com>
2020-07-29 17:15:10 +0200

    Fixed time_limit & application closing

---
+++ application/multi_app_winflector.bat
@@ -0,0 +1,41 @@
+@echo off
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
+start C:\Users\szymo\AppData\Roaming\Winflector\client\wfc.exe -addr=192.168.8.109 -user=t0 -passwd=t1234 C:\Users\szymo\Desktop\Programowanie\Projekty\app C:\Users\szymo\Desktop\Programowanie\Projekty\app\AppTester.exe
+timeout /t 1
